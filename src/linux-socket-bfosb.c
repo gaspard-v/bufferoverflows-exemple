d7ea56d2bf711c2e17f82a527eb7d6aaab99740c
Gaspard VANDECASTEELE <gaspardv@localhost.localdomain>
2022-02-10 13:24:50 +0100

    add socket

--- src/linux-socket-bfosb.c
+++ src/linux-socket-bfosb.c
@@ -1,7 +1,59 @@
 #include <stdint.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[])
+#define PORT 7777
+#define BUFFER_SIZE 256
+
+int main(int argc, char *argv[])
 {
-    
-    return 0;
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    uint8_t buffer[BUFFER_SIZE] = {0};
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                 (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket, buffer, BUFFER_SIZE);
+        printf("%s\n", buffer);
+    }
+    return EXIT_SUCCESS;
 }
