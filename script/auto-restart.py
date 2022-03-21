0d78dae5de9a856cba5a8cd90f11b967a70b2e4c
Gaspard <gaspard.vandecasteele@gmail.com>
2022-03-21 13:38:27 +0100

    change auto-restart.py

--- script/auto-restart.py
+++ script/auto-restart.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 
 def main():
     i = 0
     print("[script] Autorestart script is launched")
     while(True):
-        process = subprocess.run(['../build/linux-socket-bfosb-x64'])
+        process = subprocess.run([sys.argv[0]])
         i += 1
         print(f"[script] [{i}] Process has exited, return code: {process.returncode}")
 
